22aa961b8f7fe848d506e245068477eac8fe0a85
kaiqueBellmont <kaiquebellmont@gmail.com>
2024-04-21 12:00:00 -0300

    feat: Button  widget

--- lib/pages/basic_widgets/05_button_widget.dart
+++ lib/pages/basic_widgets/05_button_widget.dart
@@ -1,10 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_concept_practices/components/appbar/custom_appbar.dart';
 
 class ButtonWidget extends StatelessWidget {
   const ButtonWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const CustomAppBar(title: "05: Button Widget"),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: const ElevatedButton(
+                onPressed: null,
+                child: Text('This is a Button Widget!'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
